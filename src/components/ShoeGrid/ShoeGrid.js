5cbe2a5cdc34746826fa96abf7b5f6db272f46d6
Rajeev Pai <rajeevpai.code@gmail.com>
2021-12-30 15:34:09 +0530

    Add grid layout with flexbox

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -7,13 +7,26 @@ import ShoeCard from '../ShoeCard';
 const ShoeGrid = () => {
   return (
     <Wrapper>
-      {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+      {SHOES.map(shoe => (
+        <ShoeWrapper key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  /* gap: 32px; */
+  margin: -16px;
+`;
+
+const ShoeWrapper = styled.div`
+  min-width: 275px;
+  margin: 16px;
+  flex: 1;
+`;
 
 export default ShoeGrid;
